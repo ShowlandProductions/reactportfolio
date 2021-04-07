52f1a63ba71e9719b1698d7f9c8121135669fc49
Dominique Christopher <showlandproductions@gmail.com>
2021-04-06 19:31:04 -0700

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/ShowlandProductions",
+    "name": "Dominique Christopher",
+    "role": "Fullstack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I'm an aspiring Full-Stack Web Developer student at Columbia. I like learning about new ways to evolve & ways to help others around me be successful.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dominique-christopher-84374853/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ShowlandProductions",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/showlandprod",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"When I’m not keeping busy with the tasks of growing a business, you can find me working on new ways to learn & applying them daily. I started a company named Showland Productions in 2016. Since then we've done work for Yahoo Sports, DirecTv, Sports Illustrated, and HBO. My prior work experience includes DirecTv Corporate, Munselle Inc, and JLL.",
+    "address":"Los Angeles",
+    "website":"https://showlandproductions.com",
+    "education":[
+      {
+        "UniversityName":"Columbia University",
+        "specialization":"Front and Backend Development",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2021",
+        "Achievements":"Full Stack Developer"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Munselle Inc",
+        "specialization":"Company Networking & Expansion",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Successfully facilitated meetings with current and potential partners"
+      },
+      {
+        "CompanyName":"DirecTv Corporate",
+        "specialization":"Facilities Engineer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2016",
+        "Achievements":"Played a key role in renewing a major JLL contract"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Debugging"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"/images/portfolio/Quarantine-Chill-App.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/Weather-Dashboard.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/Workday-Scheduler.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/Stepping-Out_app.png"
+      },
+      {
+        "name":"project5",
+        "description":"mobileapp",
+        "imgurl":"/images/portfolio/EmployeeDirectory.jpg"
+      },
+      {
+        "name":"project6",
+        "description":"mobileapp",
+        "imgurl":"/images/portfolio/FitnessTracker.jpg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"Hardworking & extremely gifted at grasping & applying new information in real time!",
+        "name":"Weston Munselle"
+      }
+    ]
+  }
+  
+  export default resumeData
